d5bb45de88691f62b64fc2ef855477760c79c6da
sanjana <vermasanjana404@gmail.com>
2021-06-16 22:05:08 +0530

    added js

--- main/myjs.js
+++ main/myjs.js
@@ -1,5 +1,10 @@
-<script>
-function myFunction() {
+function f1() {
   alert("You clicked the picture!");
 }
-</script>
+function f2() {
+	alert("You clicked to see date & Time");
+  document.getElementById('dt').innerHTML=Date();
+	}
+function f3() {
+  document.getElementById('hi').innerHTML="How's it?";
+}
